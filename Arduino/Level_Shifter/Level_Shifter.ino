be5fa998c425ad57b4abc32da61cf1afd5591272
Reese Russell <reese.russell@ymail.com>
2019-09-18 23:53:11 -0700

    Lever Shifter Updated Further - I2C

--- Arduino/Level_Shifter/Level_Shifter.ino
+++ Arduino/Level_Shifter/Level_Shifter.ino
@@ -1,19 +1,18 @@
+#include <Wire.h>
+
 int outPin = 3;  // LED connected to digital pin 13
 int ledPin = 13;
-int inPin = 2;    // pushbutton connected to digital pin 7
-int val = 0;      // variable to store the read value
 
 // the setup function runs once when you press reset or power the board
 void setup() {
+  Wire.begin(0x8);        // join i2c bus (address optional for master)
+  digitalWrite(A4, LOW);
+  digitalWrite(A5, LOW);
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(inPin, INPUT);
   pinMode(outPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  val = digitalRead(inPin);   // read the input pin
-  digitalWrite(ledPin, val);  // sets the LED to the button's value
-  digitalWrite(outPin, val);  // sets the LED to the button's value
 }
