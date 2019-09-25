dbb77399ea696bf233747e63d8229382c996117e
Reese Russell <reese.russell@ymail.com>
2019-09-24 22:44:03 -0700

    Added Timer Time Bases
    Added Timer Time Bases so the user can best select the period they wish to use for their application. This is mainly used for the continious PWM function of sushiboard.  These Functions and Definitions Remain ONLY FOR USE WITH SUSHIBOARD or another stm32f03 with a 16MHZ HSE clock

--- Inc/sushi_timer.h
+++ Inc/sushi_timer.h
@@ -11,9 +11,20 @@
 #include "stm32f0xx_hal.h"
 #include "main.h"
 
-void signalGenCounter(uint16_t timeMS);            // Determines the time to repeat the signal... for longer runs
-void changeTimeBase(uint16_t scaler);              // Changes the Tamebase for the primary signal counter
-void gateDriveParallelPulseTimerInit(void);        // Init a timer designed to trigger all MOSFETs at one time.
-void switchInputDebouceTimerInit(uint16_t timeMS); // This time controls the deboucing timer.
+void signalGenCounter(uint16_t timeMS);             // Determines the time to repeat the signal... for longer runs
+void changeTimeBase(uint16_t scaler);               // Changes the Tamebase for the primary signal counter
+void gateDriveParallelPulseTimerInit(void);         // Init a timer designed to trigger all MOSFETs at one time.
+void switchInputDebouceTimerInit(uint16_t timeMS);  // This time controls the deboucing timer.
+void gateDriveParallelInitPWMSimpleContinuious(uint32_t  period, uint32_t dutyCycle, uint32_t timebase); // PWM Continious Initialization
+
+#define _PLL_MUL 3
+
+enum TimeBase {
+	TB_CoreClock = 1,
+	TB_1US = (HSE_VALUE * _PLL_MUL) / 1000000 - 1,
+	TB_1MS = (HSE_VALUE * _PLL_MUL)  / 1000 - 1,
+	TB_1S  = (HSE_VALUE * _PLL_MUL)  - 1,
+	TB_10S = (HSE_VALUE * _PLL_MUL)  * 10 - 1
+};
 
 #endif /* SUSHI_TIMER_H_ */
--- Src/sushi_timer.c
+++ Src/sushi_timer.c
@@ -24,13 +24,51 @@ TIM_OC_InitTypeDef tcOff;                                              // Timer 
 void changeTimeBase(uint16_t scaler){
 }
 
+void gateDriveParallelPulseTimerInitPWMSimpleContinuious(void){
+	uint16_t usPrescaler = (HSI_VALUE / 1000000) - 1;             // Number of cycles to generate 1m_pulses/sec
+	//Enabled Needed Clock Signals for the Timer perhipreal
+	__HAL_RCC_TIM1_CLK_ENABLE();
+	//Setup The Timer Parameters
+	pulseTimer1.Instance               = TIM1;                          //Using Timer 1
+	pulseTimer1.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;        //Do not divide the counter clock
+	pulseTimer1.Init.CounterMode       = TIM_COUNTERMODE_UP;            //This timer will count upwards 0,1,2,3..... Period, 0, 1 ...
+	pulseTimer1.Init.Period            = (uint16_t)sushiState.tPeriod;  //The period will be 1000 us counts before an update event DMA trigger
+	pulseTimer1.Init.Prescaler         = usPrescaler;                   //for a 16MHZ clock this needs to be 16, this will enable a 1us pulse time
+	pulseTimer1.Init.RepetitionCounter = 0;                             //No repition. Just run in a loop
+	pulseTimer1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE; //Shadow Mask.... Just enable it
+	//Setup the On Timer Channel Outputs
+	tcOn.Pulse        = (uint16_t)sushiState.tOn;                       //Time before the DMA requst is sent to the BSRR to turn on the switching GPIO
+	tcOn.OCFastMode   = TIM_OCFAST_DISABLE;                             //No need for fast mode
+	tcOn.OCMode       = TIM_OCMODE_PWM1;                                //Mode is PWM1 this mode is described in reference manual PWM2 is also
+	tcOn.OCPolarity   = TIM_OCPOLARITY_HIGH;                            //Doesn't matter but lets make the output compare polarity high
+	tcOn.OCNPolarity  = TIM_OCNPOLARITY_HIGH;                           //Doesn't matter but lets make the output compare polarity high
+	//Setup the On Timer Channel Outputs
+	tcOff.Pulse       = (uint16_t)sushiState.tOff;                      //Time before the DMA requst is sent to the BSRR to turn off the switching GPIO
+	tcOff.OCFastMode  = TIM_OCFAST_DISABLE;                             //No need for fast mode
+	tcOff.OCMode      = TIM_OCMODE_PWM1;                                //Mode is PWM1 this mode is described in reference manual PWM2 is also
+	tcOff.OCPolarity  = TIM_OCPOLARITY_HIGH;                            //Doesn't matter but lets make the output compare polarity high
+	tcOff.OCNPolarity = TIM_OCNPOLARITY_HIGH;                           //Doesn't matter but lets make the output compare polarity high
+	//Lets do Some Fun Stuff Here... DAM CC1 and 2 Events -> send the data to the BSSR registers for a pulse on and off
+	//The order and events may change in the future for now this is proof of concept.
+	__HAL_TIM_ENABLE_IT(&pulseTimer1, TIM_IT_UPDATE);                   //TIMER INTERUPT UPDATE START
+	__HAL_TIM_ENABLE_DMA(&pulseTimer1, TIM_DMA_CC1);                    //Capture Compare 1 Event (Load the Off Data)
+	__HAL_TIM_ENABLE_DMA(&pulseTimer1, TIM_DMA_CC2);                    //Capture Compare 2 Event (User event, This may not be used)
+	//sET THE INTERUPT ROUTINE PRIORITY
+	HAL_NVIC_SetPriority(TIM1_BRK_UP_TRG_COM_IRQn, 3, 0);               //Interupts .... Not using them Now; This project uses DMA GPIO
+	HAL_NVIC_EnableIRQ(TIM1_BRK_UP_TRG_COM_IRQn);                       //Enable the Interupt
+	//Start Running it;
+	HAL_TIM_PWM_Init(&pulseTimer1);                                     //Init the PWM Timer
+	HAL_TIM_PWM_ConfigChannel(&pulseTimer1, &tcOn, TIM_CHANNEL_1);      //Turn on the BSSR on the Channel one output Compare
+	HAL_TIM_PWM_ConfigChannel(&pulseTimer1, &tcOff, TIM_CHANNEL_2);     //Turn off the BSSR on the Channel two output COmpare
+}
+
 
 /**
  * @Desc: Init Timer one with interupts on UPDATE and DMA requests on CC matches to enable flipping of bits on the GPIO  BSSR registers
  * @Note: Without Adjusting period.... due to the fact the timer is limited to 16 bits 65535us or 65.535ms Max Pulse Length.
  */
 void gateDriveParallelPulseTimerInit(void){                             // 10ms Period
-	uint16_t usPrescaler = (SystemCoreClock / 1000000) - 1;             // Number of cycles to generate 1m_pulses/sec
+	uint16_t usPrescaler = (HSE_VALUE * 3 / 1000000) - 1;             // Number of cycles to generate 1m_pulses/sec
 	//Enabled Needed Clock Signals for the Timer perhipreal
 	__HAL_RCC_TIM1_CLK_ENABLE();
 	//Setup The Timer Parameters
